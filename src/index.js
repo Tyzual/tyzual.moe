097aad18590421827cc682dca494147a29dd805d
tyzual <e.tyzual@gmail.com>
2017-01-19 08:43:12 +0800

    修正格式

--- src/index.js
+++ src/index.js
@@ -1,10 +1,16 @@
 function adjustInfo() {
 	var $info = $(".info");
-	var screenWidth = $(window).width(), screenHeight = $(window).height(); //当前浏览器窗口的 宽高  
-	var scrolltop = $(document).scrollTop();//获取当前窗口距离页面顶部高度  
+	var screenWidth = $(window).width(),
+		screenHeight = $(window).height(); //当前浏览器窗口的 宽高  
+	var scrolltop = $(document).scrollTop(); //获取当前窗口距离页面顶部高度  
 	var objLeft = (screenWidth / 5 - $info.width() / 2);
 	var objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-	$info.css({ left: objLeft + 'px', top: objTop + 'px', 'display': 'block' , 'visibility': 'visible'});
+	$info.css({
+		left: objLeft + 'px',
+		top: objTop + 'px',
+		'display': 'block',
+		'visibility': 'visible'
+	});
 
 	// var closeObjLeft = (screenWidth + obj.width())/2-closeObj.width()-2 ;  
 	// var closeObjTop = (screenHeight - obj.height())/2 + scrolltop+12;  
@@ -17,7 +23,11 @@ function adjustInfo() {
 		scrolltop = $(document).scrollTop();
 		objLeft = (screenWidth / 5 - $info.width() / 2);
 		objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-		$info.css({ left: objLeft + 'px', top: objTop + 'px', 'display': 'block' });
+		$info.css({
+			left: objLeft + 'px',
+			top: objTop + 'px',
+			'display': 'block'
+		});
 	});
 
 	//浏览器有滚动条时的操作、  
@@ -27,6 +37,10 @@ function adjustInfo() {
 		scrolltop = $(document).scrollTop();
 		objLeft = (screenWidth / 5 - $info.width() / 2);
 		objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-		$info.css({ left: objLeft + 'px', top: objTop + 'px', 'display': 'block' });
+		$info.css({
+			left: objLeft + 'px',
+			top: objTop + 'px',
+			'display': 'block'
+		});
 	});
 }
