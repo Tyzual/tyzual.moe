7b3a80890ccece786768ae5ccdebf60f857d48f2
tyzual <e.tyzual@gmail.com>
2017-01-21 16:17:45 +0800

    调整动画细节

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ function adjustPoisition($info) {
 	var screenWidth = $(window).width(),
 		screenHeight = $(window).height() //当前浏览器窗口的 宽高  
 	var scrolltop = $(document).scrollTop() //获取当前窗口距离页面顶部高度  
-	if (screenWidth >= 1300) {
+	if (screenWidth / screenHeight >= 1.3) {
 		var objLeft = (screenWidth / 5 - $info.width() / 2)
 	} else {
 		var objLeft = (screenWidth / 2 - $info.width() / 2)
