dbef034ac643b940ae8e91beab184beec182698d
tyzual <e.tyzual@gmail.com>
2017-01-21 16:15:13 +0800

    完善一些细节

--- src/index.js
+++ src/index.js
@@ -1,46 +1,33 @@
-function adjustInfo() {
-	var $info = $(".info");
+function adjustPoisition($info) {
 	var screenWidth = $(window).width(),
-		screenHeight = $(window).height(); //当前浏览器窗口的 宽高  
-	var scrolltop = $(document).scrollTop(); //获取当前窗口距离页面顶部高度  
-	var objLeft = (screenWidth / 5 - $info.width() / 2);
-	var objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-	$info.css({
+		screenHeight = $(window).height() //当前浏览器窗口的 宽高  
+	var scrolltop = $(document).scrollTop() //获取当前窗口距离页面顶部高度  
+	if (screenWidth >= 1300) {
+		var objLeft = (screenWidth / 5 - $info.width() / 2)
+	} else {
+		var objLeft = (screenWidth / 2 - $info.width() / 2)
+	}
+	var objTop = (screenHeight - $info.height()) / 2 + scrolltop
+
+	$info.animate({
 		left: objLeft + 'px',
 		top: objTop + 'px',
 		'display': 'block',
 		'visibility': 'visible'
-	});
+	}, 100)
+}
 
-	// var closeObjLeft = (screenWidth + obj.width())/2-closeObj.width()-2 ;  
-	// var closeObjTop = (screenHeight - obj.height())/2 + scrolltop+12;  
-	// closeObj.css({left: closeObjLeft + 'px', top: closeObjTop + 'px','display': 'block'});  
+function adjustInfo() {
+	var $info = $(".info")
+	adjustPoisition($info)
 
 	//浏览器窗口大小改变时  
 	$(window).resize(function () {
-		screenWidth = $(window).width();
-		screenHeight = $(window).height();
-		scrolltop = $(document).scrollTop();
-		objLeft = (screenWidth / 5 - $info.width() / 2);
-		objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-		$info.css({
-			left: objLeft + 'px',
-			top: objTop + 'px',
-			'display': 'block'
-		});
-	});
+		adjustPoisition($info)
+	})
 
 	//浏览器有滚动条时的操作、  
 	$(window).scroll(function () {
-		screenWidth = $(window).width();
-		screenHeight = $(window).height();
-		scrolltop = $(document).scrollTop();
-		objLeft = (screenWidth / 5 - $info.width() / 2);
-		objTop = (screenHeight - $info.height()) / 2 + scrolltop;
-		$info.css({
-			left: objLeft + 'px',
-			top: objTop + 'px',
-			'display': 'block'
-		});
-	});
+		adjustPoisition($info)
+	})
 }
